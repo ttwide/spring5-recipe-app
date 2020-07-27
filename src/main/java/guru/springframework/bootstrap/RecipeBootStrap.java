e6bfd245f9f2fca75e58d5358f185e048f165e69
ttwide <ttwideman@hotmail.com>
2020-07-26 20:36:24 -0500

    refactor with lombok

--- src/main/java/guru/springframework/bootstrap/RecipeBootStrap.java
+++ src/main/java/guru/springframework/bootstrap/RecipeBootStrap.java
@@ -4,17 +4,21 @@ import guru.springframework.domain.*;
 import guru.springframework.repositories.CategoryRepository;
 import guru.springframework.repositories.RecipeRepository;
 import guru.springframework.repositories.UnitOfMeasureRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+@Slf4j
 @Component
 public class RecipeBootStrap implements ApplicationListener<ContextRefreshedEvent> {
+
 
     private final CategoryRepository categoryRepository;
     private final RecipeRepository recipeRepository;
@@ -28,8 +32,10 @@ public class RecipeBootStrap implements ApplicationListener<ContextRefreshedEven
 
 
     @Override
+    @Transactional
     public void onApplicationEvent(ContextRefreshedEvent event) {
-         recipeRepository.saveAll(getRecipes());
+        recipeRepository.saveAll(getRecipes());
+        log.debug("Loading BootStrap Data");
     }
 
 
--- src/main/java/guru/springframework/domain/Ingredient.java
+++ src/main/java/guru/springframework/domain/Ingredient.java
@@ -1,9 +1,15 @@
 package guru.springframework.domain;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
 import javax.persistence.*;
 import java.math.BigDecimal;
 
+
 @Entity
+@EqualsAndHashCode(exclude = {"recipe"})
+@Data
 public class Ingredient {
 
     @Id
@@ -34,43 +40,4 @@ public class Ingredient {
         this.recipe = recipe;
     }
 
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public BigDecimal getAmount() {
-        return amount;
-    }
-
-    public void setAmount(BigDecimal amount) {
-        this.amount = amount;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public UnitOfMeasure getUom() {
-        return uom;
-    }
-
-    public void setUom(UnitOfMeasure uom) {
-        this.uom = uom;
-    }
-
-    public Recipe getRecipe() {
-        return recipe;
-    }
-
-    public void setRecipe(Recipe recipe) {
-        this.recipe = recipe;
-    }
 }
